953743c246739c61f418e14bc59e6d810297cb84
Michael Maslennikov <mih.mass@yandex.ru>
2023-03-25 15:03:55 +0500

    2.3 assignment done

--- app/src/main/java/ru/mihmas/aston_2/MainActivity.kt
+++ app/src/main/java/ru/mihmas/aston_2/MainActivity.kt
@@ -1,11 +1,72 @@
 package ru.mihmas.aston_2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
+import ru.mihmas.aston_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val bind: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(bind.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        bind.openWebsiteButton.setOnClickListener {
+            openWebSite()
+        }
+        bind.openLocationButton.setOnClickListener {
+            openLocation()
+        }
+        bind.shareTextButton.setOnClickListener {
+            shareText()
+        }
+        bind.cameraButton.setOnClickListener {
+            startCamera()
+        }
+    }
+
+    private fun openWebSite() {
+        val url = bind.websiteEdittext.text.toString()
+        val webpage = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!")
+        }
+    }
+
+    private fun openLocation() {
+        val loc = bind.locationEdittext.text.toString()
+        val addressUri = Uri.parse("geo:0,0?q=$loc")
+        val intent = Intent(Intent.ACTION_VIEW, addressUri)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!")
+        }
+    }
+    private fun shareText() {
+        val txt = bind.shareEdittext.text.toString()
+        val mimeType = "text/plain"
+        ShareCompat.IntentBuilder
+            .from(this)
+            .setType(mimeType)
+            .setChooserTitle("Share this text with: ")
+            .setText(txt)
+            .startChooser()
+    }
+
+    private fun startCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivity(intent)
     }
 }
